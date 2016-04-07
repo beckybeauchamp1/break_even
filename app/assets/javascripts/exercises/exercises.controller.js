c6d54b386acad866548af676e19a9eb3fa56420c
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-06 21:44:18 -0400

    angular bail - pair with matt on how to sneak in some js and jquery for last minute functionality

--- app/assets/javascripts/exercises/exercises.controller.js
+++ app/assets/javascripts/exercises/exercises.controller.js
@@ -12,5 +12,13 @@
   function ExerciseIndexControllerFunction(ExerciseFactory, DayFactory){
     this.exercises = ExerciseFactory.query();
     this.days = DayFactory.query();
+    this.totalCalories = function() {
+      var totalCalories = 0;
+      this.exercises.forEach(function(exercise, index){
+        totalCalories += exercise.calories_burned;
+      })
+      return totalCalories;
+    }
+    this.beers = [{name: 'Coors', calories: 90}, {name: "Molson", calories: 100}]
   }
 }());
